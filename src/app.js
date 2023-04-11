caa05af2424ace09c98459863bf0aa3bab2fb511
TueTran <{tuemeoa13@gmail.com}>
2023-04-11 15:09:28 +0700

    add allow cors fe

--- src/app.js
+++ src/app.js
@@ -3,8 +3,14 @@ const morgan = require("morgan");
 const { default: helmet } = require("helmet");
 const compression = require("compression");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
-
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 // init middlewares
 app.use(morgan("dev"));
 app.use(helmet());
@@ -16,6 +22,32 @@ app.use(express.urlencoded({ extended: true }));
 require("./dbs/init.mongodb");
 // const { checkOverLoad } = require('./helpers/check.connect')
 // checkOverLoad()
+
+// cors
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+//
 
 //INIT ROUTES
 app.use("/", require("./routes"));
@@ -31,7 +63,7 @@ app.use((error, req, res, next) => {
   return res.status(statusCode).json({
     status: "error",
     code: statusCode,
-    stack: error.stack , 
+    stack: error.stack,
     message: error.message || "Internal server error",
   });
 });
